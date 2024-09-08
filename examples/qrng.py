fdaf053d6082f023775bd2ae40f250bfebaeb1e0
Alexandr <a.timoff3@gmail.com>
2024-09-08 17:20:38 +0300

    add sample for qrng

--- examples/qrng.py
+++ examples/qrng.py
@@ -1,7 +1,15 @@
 from core.interface import QuantumDevice
+from core.simulator import SingleQubitSimulator
 
 
 def qrng(device: QuantumDevice) -> bool:
     with device.using_qubit() as q:
         q.h()
         return q.measure()
+
+
+if __name__ == '__main__':
+    qsim = SingleQubitSimulator()
+    for i in range(10):
+        sample = qrng(qsim)
+        print(f'[x] QRNG value is: {int(sample)}')
