ea618d94dae1fdf80e17f7b72bb18adbd1d588fd
timofeevAS <a.timoff3@gmail.com>
2024-09-24 16:06:45 +0300

    add eva to hack key

--- examples/bb84.py
+++ examples/bb84.py
@@ -4,6 +4,12 @@ from typing import List
 from core.interface import QuantumDevice
 from core.simulator import SimulatedQubit, SingleQubitSimulator
 
+EVA_STORAGE: List[SimulatedQubit] = []
+EVA_KEY = []
+ALICE_KEY = []
+BOB_KEY = []
+ALICE_BASIS = []
+BOB_BASIS = []
 
 def sample_random_bit(device: QuantumDevice) -> bool:
     with device.using_qubit() as q:
@@ -34,29 +40,44 @@ def convert_to_hex(bits: List[bool]) -> str:
         2
     ))
 
+def eva_attack(q: SimulatedQubit, eva_device: QuantumDevice) -> SimulatedQubit:
+    EVA_STORAGE.append(q.copy())
+    [eva_message, eva_basis] = [
+        sample_random_bit(eva_device) for _ in range(2)
+    ]
+    prepare_message_qubit(eva_message, eva_basis, q)
+    return q
+
+
 def send_single_bit_with_bb84(
         alice_device: QuantumDevice,
-        bob_device: QuantumDevice
-) -> tuple:
-    [your_message, your_basis] = [
+        bob_device: QuantumDevice,
+        eva_device: QuantumDevice) -> tuple:
+    [alice_message, alice_basis] = [
         sample_random_bit(alice_device) for _ in range(2)
     ]
+    ALICE_BASIS.append(alice_basis)
 
     bob_basis = sample_random_bit(bob_device)
+    BOB_BASIS.append(bob_basis)
 
+    q_copy: SimulatedQubit
+    q: SimulatedQubit
     with alice_device.using_qubit() as q:
-        prepare_message_qubit(your_message, your_basis, q)
+        prepare_message_qubit(alice_message, alice_basis, q)
+        q_copy = q.copy()
 
-        # QUBIT SENDING...
+    q = eva_attack(q_copy, eva_device)
 
-        bob_result = measure_message_qubit(bob_basis, q)
+    bob_result = measure_message_qubit(bob_basis, q)
 
-    return (your_message, your_basis), (bob_result, bob_basis)
+    return (alice_message, alice_basis), (bob_result, bob_basis)
 
 
 def simulate_bb84(n_bits: int) -> list:
     alice_device = SingleQubitSimulator()
     bob_device = SingleQubitSimulator()
+    eva_device = SingleQubitSimulator()
 
     key = []
     n_rounds = 0
@@ -64,11 +85,13 @@ def simulate_bb84(n_bits: int) -> list:
     while len(key) < n_bits:
         n_rounds += 1
         ((alice_bit, alice_basis), (bob_result, bob_basis)) = \
-            send_single_bit_with_bb84(alice_device, bob_device)
+            send_single_bit_with_bb84(alice_device, bob_device, eva_device)
 
         if alice_basis == bob_basis:
-            assert alice_bit == bob_result
+            # assert alice_bit == bob_result:
+            EVA_KEY.append(measure_message_qubit(alice_basis,EVA_STORAGE[-1]))
             key.append(alice_bit)
+            BOB_KEY.append(bob_result)
 
     print(f"Took {n_rounds} rounds to generate a {n_bits}-bit key.")
 
@@ -84,15 +107,18 @@ def apply_one_time_pad(message: List[bool], key: List[bool]) -> List[bool]:
 
 if __name__ == "__main__":
     print("Generating a 100-bit key by simulating BB84...")
-    message = list(map(bool, [random.randint(0, 1) for _ in range(100)]))
-    key = simulate_bb84(100)
-    print(f"Got key                           {convert_to_hex(key)}.")
+    SIZE=20
+    message = list(map(bool, [random.randint(0, 1) for _ in range(SIZE)]))
+    key = simulate_bb84(SIZE)
+    print(f"Eva key                           {convert_to_hex(EVA_KEY)}.")
+    print(f"Bob key                           {convert_to_hex(BOB_KEY)}.")
+    print(f"Alice key                         {convert_to_hex(key)}.")
     print(f"Using key to send secret message: {convert_to_hex(message)}.")
 
     encrypted_message = apply_one_time_pad(message, key)
     print(f"Encrypted message:                {convert_to_hex(encrypted_message)}.")
 
-    decrypted_message = apply_one_time_pad(encrypted_message, key)
-    print(f"Bob decrypted to get:             {convert_to_hex(decrypted_message)}.")
-
-    assert convert_to_hex(message) == convert_to_hex(decrypted_message)
+    decrypted_message_bob = apply_one_time_pad(encrypted_message, BOB_KEY)
+    decrypted_message_eva = apply_one_time_pad(encrypted_message, EVA_KEY)
+    print(f"Bob decrypted to get:             {convert_to_hex(decrypted_message_bob)}.")
+    print(f"Eva decrypted to get:             {convert_to_hex(decrypted_message_eva)}.")
