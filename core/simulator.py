85a78c28be912959eb85f96b37bead5f93932f2a
timofeevAS <a.timoff3@gmail.com>
2024-10-01 16:36:54 +0300

    add implementation for quantum N simulator

--- core/simulator.py
+++ core/simulator.py
@@ -1,7 +1,9 @@
 import numpy as np
+import itertools
 
 from core.basic import H, KET_0, PAULI_X, PAULI_Y, PAULI_Z, CNOT
 from core.interface import QubitInterface, QuantumDevice
+
 
 
 class SimulatedQubit(QubitInterface):
@@ -131,3 +133,66 @@ class TwoQubitSimulator(QuantumDevice):
         Set the state of the two qubits manually.
         """
         self.state = state
+
+class NQubitSimulator:
+    def __init__(self, n: int):
+        available_qubits = [SimulatedQubit() for _ in range(n)]
+        self.state = KET_0
+        self.dimension = n
+
+        for i in range(1, n):
+            self.state = np.kron(self.state, available_qubits[i])
+
+    def apply_single_qubit_gate(self, gate, qubit_idx: int):
+        operation = None
+        if qubit_idx == 0:
+            # Generate operation matrix: GATE x I x I x ... x I
+            operation = gate
+            for i in range(0, self.dimension):
+                operation = np.kron(operation, np.eye(2))
+        elif qubit_idx == self.dimension - 1:
+            # Generate operation matrix: I x I x ... x G x I x ... x I
+            operation = np.eye(2)
+            for i in range(0, self.dimension - 1):
+                operation = np.kron(operation, np.eye(2))
+            operation = np.kron(operation, gate)
+        elif 0 < qubit_idx < self.dimension - 1:
+            # Generate operation matrix: I x I x ...x I x G
+            operation = np.eye(2)
+            for i in range(0, qubit_idx - 1):
+                operation = np.kron(operation, np.eye(2))
+            operation = np.kron(operation, gate)
+            for i in range(qubit_idx + 1, self.dimension):
+                operation = np.kron(operation, np.eye(2))
+        else:
+            raise ValueError(f"Invalid qubit index. Only in range [0;{self.dimension}).")
+
+        # Apply operation to quantum state.
+        self.state = operation @ self.state
+
+    def apply_n_qubit_gate(self, gate):
+        # TODO: Is it need to check dimension?
+        self.state = gate @ self.state
+
+    def measure(self, qubit_idx: int) -> bool:
+        """
+        Measure the state of the specified qubit.
+        """
+        if qubit_idx > self.dimension or qubit_idx < 0:
+            raise ValueError(f"Invalid qubit index. Only in range [0;{self.dimension}).")
+
+        # We'll just measure individual qubits by marginalizing over the other qubit.
+        prob_zero = 0
+        combinations = list(itertools.product([0, 1], repeat=self.dimension))
+
+        for i in range(len(combinations)):
+            if combinations[qubit_idx] == 0:
+                prob_zero += np.abs(self.state[i, 0]) ** 2
+
+        is_measured_zero = np.random.random() <= prob_zero
+        return bool(0 if is_measured_zero else 1)
+
+    def reset(self):
+        self.state = KET_0
+        for i in range(self.dimension):
+            self.state = np.kron(self.state, KET_0)
