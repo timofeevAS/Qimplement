d915d22409249d7f572b59e832db53dff9bfc8b6
timofeevAS <a.timoff3@gmail.com>
2024-10-07 23:50:57 +0300

    add generator for bernstein_vazirani

--- core/oracle.py
+++ core/oracle.py
@@ -36,6 +36,45 @@ def generate_oracle_deutsch_jozsa(N, boolean_function):
 
     return oracle_matrix
 
+import numpy as np
+import itertools
+
+def generate_oracle_bernstein_vazirani(N, s):
+    """
+    Generate Oracle matrix for Bernstein-Vazirani problem.
+
+    Args:
+    - N: Number of variables.
+    - s: Hidden string used for scalar product in the boolean function.
+
+    Returns:
+    - Matrix of 2^(N+1) x 2^(N+1), describes Oracle.
+    """
+    # Dimension of Oracle matrix
+    matrix_size = 2 ** (N + 1)
+
+    # Init zero matrix
+    oracle_matrix = np.zeros((matrix_size, matrix_size), dtype=int)
+
+    # Iterate over all possible inputs (x) and auxiliary bit (y)
+    for x in itertools.product([0, 1], repeat=N):
+        for y in [0, 1]:  # y - auxiliary qubit
+            # Input index (binary representation of x followed by y)
+            input_index = int(''.join(map(str, x)) + str(y), 2)
+
+            # Calculate the boolean function f(x) = s · x mod 2 (scalar product)
+            f_x = np.dot(s, x) % 2
+
+            # Calculate the output value for the auxiliary bit
+            output_y = y ^ f_x  # y XOR f(x)
+
+            # Output index (binary representation of x followed by new y)
+            output_index = int(''.join(map(str, x)) + str(output_y), 2)
+
+            # Set the matrix element at (input_index, output_index) to 1
+            oracle_matrix[input_index][output_index] = 1
+
+    return oracle_matrix
 
 if __name__ == '__main__':
     # Example
@@ -53,3 +92,13 @@ if __name__ == '__main__':
     N = 2
     oracle = generate_oracle_deutsch_jozsa(N, xor)
     print(np.array(oracle))
+
+    # Example Bernstein-Vazirani
+    # Example: hidden string s = [1, 0] (N = 2)
+    print('Bernstein Vazirani:')
+    s = [1, 0]
+    N = len(s)
+
+    oracle = generate_oracle_bernstein_vazirani(N, s)
+    print(np.array(oracle))
+
