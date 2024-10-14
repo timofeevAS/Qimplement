e4aae50df3326be2742d69088c34841c0004df2e
timofeevAS <a.timoff3@gmail.com>
2024-10-14 12:52:51 +0300

    use correct name for const true function

--- examples/deutsch_jozsa.py
+++ examples/deutsch_jozsa.py
@@ -60,10 +60,10 @@ if __name__ == '__main__':
 
 
     # Example of Deutsch-Jozsa algorithm for N = 3 with Oracle (const ONE)
-    def bool_and(x) -> bool:
+    def bool_true(x) -> bool:
         return True
 
-    oracle = generate_oracle_deutsch_jozsa(2, bool_and)
+    oracle = generate_oracle_deutsch_jozsa(2, bool_true)
 
     print('Run with next Oracle (CONST TRUE):')
     print(np.array(oracle))
